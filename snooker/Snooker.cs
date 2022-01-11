9a68800266d1f5001df195699189d51e498626f3
Soldier46 <rschuszler46@gmail.com>
2022-01-11 09:40:18 +0100

    5.

--- snooker/Snooker.cs
+++ snooker/Snooker.cs
@@ -13,7 +13,7 @@ namespace snooker
         static void Main(string[] args)
         {
             List<Versenyző> versenyzők = new List<Versenyző>();
-                foreach (var sor in File.ReadAllLines("snooker.txt").Skip(1))
+            foreach (var sor in File.ReadAllLines("snooker.txt").Skip(1))
             {
                 versenyzők.Add(new Versenyző(sor));
             }
@@ -32,8 +32,40 @@ namespace snooker
             Console.WriteLine($"4.feladat A versenyzők átlagosan {átlag:0.00} fontot kerestek");
 
 
-            //4.
+            //4.  
             Console.WriteLine($"4b.feladat A versenyzők átlagosan {versenyzők.Average(x => x.Nyeremény):0.00} fontot kerestek");
+
+            //5.
+
+            int maxNyer = 0;
+            Versenyző maxV = versenyzők[0];
+            foreach (var v in versenyzők)
+            {
+                if (v.Ország == "Kína" && v.Nyeremény > maxV.Nyeremény)
+                {
+                    maxV = v;
+                }
+
+            }
+            Console.WriteLine($"legjobban kereső kínai versenyző");
+            Console.WriteLine($"\tHelyezés: {maxV.Helyezés}");
+            Console.WriteLine($"\tNév: {maxV.Név}");
+            Console.WriteLine($"\tOrszág: {maxV.Ország}");
+            Console.WriteLine($"\tNyeremény: {(maxV.Nyeremény  * 380).ToString("C0")} ");
+
+
+            //vagy
+            maxV = versenyzők
+            .Where(v => v.Ország == "kína")
+            .OrderBy(v => v.Nyeremény)
+            .Last();
+            Console.WriteLine($"legjobban kereső kínai versenyző");
+            Console.WriteLine($"\tHelyezés: {maxV.Helyezés}");
+            Console.WriteLine($"\tNév: {maxV.Név}");
+            Console.WriteLine($"\tOrszág: {maxV.Ország}");
+            Console.WriteLine($"\tNyeremény: {(maxV.Nyeremény * 380).ToString("C0")} ");
+
+
         }
     }
 }
